d694f4490baec2dad3f164eb445ed3fd53c72c7f
DaveX <dave.fang@outlook.com>
2016-01-15 14:33:59 -0700

    Accepted 71.19% (Mine)

--- 169_Majority_Element/solution_1st.py
+++ 169_Majority_Element/solution_1st.py
@@ -1,1 +1,10 @@
 
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums_sorted = sorted(nums)
+        n = len(nums)/2
+        return nums_sorted[n]
