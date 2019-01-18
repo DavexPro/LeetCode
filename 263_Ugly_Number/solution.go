d164d2118e5d2132c1149f93c53cade1b34ac5de
Dave Fang <dave.fang@outlook.com>
2019-01-18 16:54:43 +0800

    Accepted 100% (Ref)
    这题理解不够深刻，如果是 Ugly Number 的话，肯定会被 2, 3, 5 除尽，最后肯定就只剩 1 了。

---
+++ 263_Ugly_Number/solution.go
@@ -0,0 +1,18 @@
+func isUgly(num int) bool {
+    if num < 1 {
+        return false
+    }
+    
+    if num == 1{
+        return true
+    }
+    
+    primes := [3]int{5, 3, 2}
+    for _, p := range primes {
+        for (num % p) == 0 {
+            num /= p
+        }
+    }
+    
+    return num == 1
+}
