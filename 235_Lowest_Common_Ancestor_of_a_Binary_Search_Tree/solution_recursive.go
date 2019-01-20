5e67e462425d2703cdbe5af29871d253f303c918
Dave Fang <dave.fang@outlook.com>
2019-01-20 18:13:05 +0800

    Accepted 100% (Recursive)
    BST 的最低公共祖先，递归版本

---
+++ 235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/solution_recursive.go
@@ -0,0 +1,21 @@
+/**
+* Definition for TreeNode.
+* type TreeNode struct {
+*     Val int
+*     Left *ListNode
+*     Right *ListNode
+* }
+*/
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    if root == nil || p == nil || q == nil {
+        return nil
+    }
+
+    if p.Val < root.Val && q.Val < root.Val {
+        return lowestCommonAncestor(root.Left, p, q)
+    }else if p.Val > root.Val && q.Val > root.Val {
+        return lowestCommonAncestor(root.Right, p, q)
+    }
+    
+    return root
+}
